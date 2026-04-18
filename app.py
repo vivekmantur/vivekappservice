1fd291214b092845e3e6a49ca61facad1f861216
vivekmantur <vivek.mantur@cognine.com>
2026-04-18 16:02:18 +0530

    code-changes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Vivek Azure Web App 🚀"
+
+@app.route("/env")
+def get_env():
+    value = os.getenv("MY_MESSAGE", "No env variable found")
+    return f"ENV VALUE: {value}"
